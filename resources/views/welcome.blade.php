a075e78c70978fe688e65811f11c22e117b2bcc8
Paola Miramontes <57874374+PaoConcepcion@users.noreply.github.com>
2022-02-03 14:08:35 -0600

    prueba mágica

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Esto estuvo muy facil como para que sea verdad hasta ahorita whut?
+                                    Un mensajin nuevo para que se vea que si se subio esta wea
                                 </div>
                             </div>
                         </div>
